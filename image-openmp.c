89501b5a0258d3b5fe7ef175ea15624085a8d9c7
Jan Ahmed <janahmed@ubuntu-2004.eecis.udel.edu>
2023-10-30 18:25:53 -0400

    done

--- image-openmp.c
+++ image-openmp.c
@@ -2,7 +2,8 @@
 #include <stdint.h>
 #include <time.h>
 #include <string.h>
-#include "image.h"
+#include "image-openmp.h"
+#include <omp.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -91,6 +92,7 @@ enum KernelTypes GetKernelType(char* type){
 //argv is expected to take 2 arguments.  First is the source file name (can be jpg, png, bmp, tga).  Second is the lower case name of the algorithm.
 int main(int argc,char** argv){
     long t1,t2;
+    int thread_count = 4;
     t1=time(NULL);
 
     stbi_set_flip_vertically_on_load(0); 
@@ -111,6 +113,7 @@ int main(int argc,char** argv){
     destImage.height=srcImage.height;
     destImage.width=srcImage.width;
     destImage.data=malloc(sizeof(uint8_t)*destImage.width*destImage.bpp*destImage.height);
+#   pragma omp parallel num_threads(thread_count)
     convolute(&srcImage,&destImage,algorithms[type]);
     stbi_write_png("output.png",destImage.width,destImage.height,destImage.bpp,destImage.data,destImage.bpp*destImage.width);
     stbi_image_free(srcImage.data);
--- makefile
+++ makefile
@@ -1,7 +1,9 @@
 
 image:image.c image.h
 	gcc -g image.c -o image -lm
-ptimage:image.c image.h
+ptimage:image-pthread.c image-pthread.h
 	gcc -g image.c -o image -lm -lpthread
+mpimage:image-openmp.c image-openmp.h
+	gcc -g image.c -o image -lm -fopenmp
 clean:
 	rm -f image output.png
